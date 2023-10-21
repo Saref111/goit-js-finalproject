cccc6695675892ce16653a8f70e98f80bbb4360b
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-10-21 13:01:56 +0300

    added main list

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const createGalleryElements = (galleryItems) => {
+    return galleryItems.map(({ preview, original, description }) => {
+        const galleryElement = `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+            </li>
+        `;
+        return galleryElement;
+    })
+}
+
+const createGallery = (galleryItems) => {
+    const galleryElements = createGalleryElements(galleryItems);
+
+    const gallery = document.querySelector('.gallery');
+    gallery.insertAdjacentHTML('beforeend', galleryElements);
+}
+
+createGallery(galleryItems);
