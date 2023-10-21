ab80ed8c90ea5c0954cd7c484ca2a52daa34fce1
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-10-21 13:16:47 +0300

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,7 +6,7 @@ const createGalleryElements = (galleryItems) => {
         const galleryElement = `
             <li class="gallery__item">
                 <a class="gallery__link" href="${original}">
-                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
                 </a>
             </li>
         `;
@@ -14,11 +14,23 @@ const createGalleryElements = (galleryItems) => {
     })
 }
 
+const onGalleryClick = (event) => {
+    event.preventDefault();
+    const galleryImage = event.target;
+    if (galleryImage.nodeName !== 'IMG') return;
+
+    const instance = basicLightbox.create(`
+        <img src="${galleryImage.dataset.source}" width="800" height="600">
+    `)
+    instance.show();
+}
+
 const createGallery = (galleryItems) => {
     const galleryElements = createGalleryElements(galleryItems);
 
     const gallery = document.querySelector('.gallery');
-    gallery.insertAdjacentHTML('beforeend', galleryElements);
+    gallery.insertAdjacentHTML('beforeend', galleryElements.join(''));
+    gallery.addEventListener('click', onGalleryClick);
 }
 
 createGallery(galleryItems);
